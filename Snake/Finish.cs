d13825b861dde50232e1c1b40bcd59593f170c17
White78Wolf <cepkov78@mail.ru>
2018-07-13 16:40:14 +0300

    Added class Finish.cs with Finish.GameOver() and Finish.WannaToExit, also the game can be restarted

--- Snake/Finish.cs
+++ Snake/Finish.cs
@@ -8,6 +8,8 @@ namespace Snake
 {
     class Finish
     {
+        public static bool WannaToExit = false;        
+
         public static void GameOver()
         {
             Console.Clear();
@@ -21,8 +23,31 @@ namespace Snake
             Thread.Sleep(1000);
 
             Console.SetCursorPosition(29, 13);
-            Console.WriteLine("Press Enter to quit...");
-            Console.ReadKey();
+            Console.WriteLine("Press Enter to restart...");
+
+            Console.SetCursorPosition(32, 14);
+            Console.WriteLine("...or Esc to quit");
+
+            //Console.ReadKey();
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            while (key.Key != ConsoleKey.Escape && key.Key != ConsoleKey.Enter)
+            {
+                key = Console.ReadKey();                
+            }
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                WannaToExit = true;                
+            }
+
+            if (key.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }              
         }        
     }
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,42 +12,53 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-
-            Walls walls = new Walls(80, 25);
-            walls.Draw();
-
-            // Points drawing
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
-
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+            Walls walls = new Walls(80, 25);            
 
             while (true)
             {
-                if (walls.IsHit(snake) || snake.IsHitTail())
-                    break;
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
+                walls.Draw();
 
-                Thread.Sleep(100);
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HadleKey(key.Key);
-                }
-            }
+                // Points drawing
+                Point p = new Point(4, 5, '*');
+                Snake snake = new Snake(p, 4, Direction.RIGHT);
+                snake.Draw();
 
-            Finish.GameOver();
+                FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+                Point food = foodCreator.CreateFood();
+                food.Draw();
+
+                while (true)
+                {
+                    if (walls.IsHit(snake) || snake.IsHitTail())
+                        break;
+
+                    if (snake.Eat(food))
+                    {
+                        food = foodCreator.CreateFood();
+                        food.Draw();
+                    }
+                    else
+                    {
+                        snake.Move();
+                    }
+
+                    Thread.Sleep(100);
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey();
+                        
+                        if (key.Key == ConsoleKey.Escape)
+                            break;
+
+                        snake.HadleKey(key.Key);                            
+                    }
+                }               
+                                
+                Finish.GameOver();
+
+                if (Finish.WannaToExit == true)
+                    break;                
+            }                       
         }
     }
 }
